8079fe45d83618da460931757b7e484db0d008b3
glucy <69760367+glucyzz@users.noreply.github.com>
2026-01-09 18:59:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -228,37 +228,6 @@ async function ADD(envadd) {
 }
 
 async function nginx() {
-	const text = `
-	<!DOCTYPE html>
-	<html>
-	<head>
-	<title>Welcome to nginx!</title>
-	<style>
-		body {
-			width: 35em;
-			margin: 0 auto;
-			font-family: Tahoma, Verdana, Arial, sans-serif;
-		}
-	</style>
-	</head>
-	<body>
-	<h1>Welcome to nginx!</h1>
-	<p>If you see this page, the nginx web server is successfully installed and
-	working. Further configuration is required.</p>
-	
-	<p>For online documentation and support please refer to
-	<a href="http://nginx.org/">nginx.org</a>.<br/>
-	Commercial support is available at
-	<a href="http://nginx.com/">nginx.com</a>.</p>
-	
-	<p><em>Thank you for using nginx.</em></p>
-	</body>
-	</html>
-	`
-	return text;
-}
-
-async function nginx() {
   const text = `
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -882,3 +851,4 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 	}
 
 }
+
